27e7ffb58fdec94c033f0dd9362b02c86dfb7e9e
Ouzii <oskari.laaja@gmail.com>
2019-11-21 12:58:26 +0200

    Break folder structure

--- App.js
+++ App.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Button>Summon</Button>
     </View>
   );
 }
